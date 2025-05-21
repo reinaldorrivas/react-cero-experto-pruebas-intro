771f6c19cae79174f879acc750c60366d72e861c
Rene Rivas <rrivas@teravisiontech.com>
2025-05-21 17:02:38 -0400

    fh-react-jest-intro: Initial commit

---
+++ src/base-pruebas/09-promesas.js
@@ -0,0 +1,16 @@
+import { getHeroeById } from "./bases/08-imp-exp";
+
+const getHeroeByIdAsync = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const p1 = getHeroeById(id);
+      if (p1) {
+        resolve(p1);
+      } else {
+        reject("No se pudo encontrar el héroe");
+      }
+    }, 2000);
+  });
+};
+
+getHeroeByIdAsync(1).then(console.log).catch(console.warn);
