2fef469a515a85dda40c0629559349e1b3908fce
Rene Rivas <rrivas@teravisiontech.com>
2025-05-29 12:14:13 -0400

    fh-react-jest-intro: First react component test

--- __tests__/FirstApp.test.jsx
+++ __tests__/FirstApp.test.jsx
@@ -1,3 +1,14 @@
+import { render } from "@testing-library/react";
+import { FirstApp } from "../src/FirstApp";
+
 describe("Prueba Unitaria - FirstApp:", () => {
-  test("Debe coincidir con la captura (Snapshot):", () => {});
+  test("Debe coincidir con la captura (Snapshot):", () => {
+    render(
+      <FirstApp
+        title="¡Hola a todos!"
+        subTitle="¡Toca estudiar!"
+        name="René Rivas"
+      />
+    );
+  });
 });
