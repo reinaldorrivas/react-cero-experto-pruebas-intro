d55c1f90d08cdb6defd9bfe7923352ee21128a2b
Rene Rivas <rrivas@teravisiontech.com>
2025-05-28 22:45:24 -0400

    fh-react-jest-intro: Added components to test in jest

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,3 @@
+export const HelloWorldApp = () => {
+  return <h1>Hello World App</h1>;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CounterApp } from "./CounterApp";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CounterApp value={20} />
+  </React.StrictMode>
+);
