83a30949cd9059e4f5ea9bc87cb109ba7516ebc2
Rene Rivas <rrivas@teravisiontech.com>
2025-06-06 19:38:53 -0400

    fh-react-jest-intro: First App refinement and delete files that are in the .gitignore to avoid push those files in the future.

--- __tests__/FirstApp.test.jsx
+++ __tests__/FirstApp.test.jsx
@@ -1,12 +1,12 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { FirstApp } from "../src/FirstApp";
 
 describe("Prueba Unitaria - FirstApp:", () => {
-  test("Debe coincidir con la captura (Snapshot):", () => {
-    const title = "¡Hola a todos!";
-    const subTitle = "¡Toca estudiar!";
-    const name = "René Rivas";
+  const title = "¡Hola a todos!";
+  const subTitle = "¡Toca estudiar!";
+  const name = "René Rivas";
 
+  test("Debe coincidir con la captura (Snapshot):", () => {
     const { container } = render(
       <FirstApp title={title} subTitle={subTitle} name={name} />
     );
@@ -15,36 +15,33 @@ describe("Prueba Unitaria - FirstApp:", () => {
   });
 
   test("Debe de existir el texto para el título:", () => {
-    const title = "¡Hola a todos!";
-    const subTitle = "¡Toca estudiar!";
-    const name = "René Rivas";
+    render(<FirstApp title={title} subTitle={subTitle} name={name} />);
 
-    const { getByText } = render(
-      <FirstApp title={title} subTitle={subTitle} name={name} />
-    );
+    const { getByText } = screen;
 
     expect(getByText(title)).toBeTruthy();
   });
 
-  test("Debe de estar el tìtulo en la etiqueta correspondiente", () => {
-    const title = "¡Hola a todos!";
-    const subTitle = "¡Toca estudiar!";
-    const name = "René Rivas";
+  test("Debe de existir el texto para el título en un <h1></h1>:", () => {
+    render(<FirstApp title={title} subTitle={subTitle} name={name} />);
 
-    const { getByTestId } = render(
-      <FirstApp title={title} subTitle={subTitle} name={name} />
-    );
+    const { getByRole } = screen;
+
+    expect(getByRole("heading", { level: 1 }).innerHTML).toBeTruthy();
+  });
+
+  test("Debe de estar el tìtulo en la etiqueta correspondiente", () => {
+    render(<FirstApp title={title} subTitle={subTitle} name={name} />);
+
+    const { getByTestId } = screen;
 
     expect(getByTestId("data-title").innerHTML).toContain(title);
   });
-  test("Debe de existir múltiples concurrencias del subtítulo:", () => {
-    const title = "¡Hola a todos!";
-    const subTitle = "¡Toca estudiar!";
-    const name = "René Rivas";
 
-    const { getAllByText } = render(
-      <FirstApp title={title} subTitle={subTitle} name={name} />
-    );
+  test("Debe de existir múltiples concurrencias del subtítulo:", () => {
+    render(<FirstApp title={title} subTitle={subTitle} name={name} />);
+
+    const { getAllByText } = screen;
 
     expect(getAllByText(subTitle).length).toBeGreaterThan(1);
   });
